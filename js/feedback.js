97f650b346ecbff1078f4d57bce630df0d3709ec
phamvinhquang97 <phamvinhquang77@gmail.com>
2021-10-17 18:24:58 +0930

    JS for feedback page

---
+++ js/feedback.js
@@ -0,0 +1,27 @@
+var form = document.getElementById("my-form");
+
+    
+    async function handleSubmit(event) {
+      event.preventDefault(); // prevent link to the form page (Form spree.com)
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        form.reset(); // reset the form
+        status.style.display = "block"; // display the message
+        status.style.color = "#4F8A10";
+        status.style.background= "#DFF2BF";
+        status.innerHTML = "Submit successfully ! Thank you."; // this message will be display if the user submitting successfully.
+      }).catch(error => {
+        status.style.display = "block"; // display the message.
+        status.style.color = "#D8000C"; 
+        status.style.background= "#FFD2D2";
+        status.innerHTML = "Oops! There was a problem submitting your form" // this message will be shown if error in submit the form.
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
